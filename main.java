965400cf637d2948c73feb4a0f2eb15aa0521bfa
axel-osoft <75009084+axel-osoft@users.noreply.github.com>
2024-04-02 10:24:53 +0200

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, New World!"); 
+        System.out.println("Hello, New Java World!"); 
+        
     }
 }
